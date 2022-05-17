5e0079f4c5a95e17981bf597ce0051de57397a24
pmns5 <p.mansi5@studenti.unisa.it>
2022-05-17 18:32:57 +0200

    updates

--- Gomoku.py
+++ Gomoku.py
@@ -1,12 +1,263 @@
-import numpy as np
-import pygame
+from TickTacToe.games import *
 
-class Game:
-    def __init__(self, size):
+import random
+import pygame
+import numpy as np
+import itertools
+import sys
+import networkx as nx
+from pygame import gfxdraw
+
+# Game constants
+BOARD_BROWN = (199, 105, 42)
+BOARD_WIDTH = 700
+BOARD_BORDER = 75
+STONE_RADIUS = 22
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+TURN_POS = (BOARD_BORDER, 20)
+SCORE_POS = (BOARD_BORDER, BOARD_WIDTH - BOARD_BORDER + 30)
+DOT_RADIUS = 4
+
+PLAYER_BLACK = 1
+PLAYER_WHITE = 2
+
+
+def make_grid(size):
+    """Return list of (start_point, end_point pairs) defining gridlines
+    Args:
+        size (int): size of grid
+    Returns:
+        Tuple[List[Tuple[float, float]]]: start and end points for gridlines
+    """
+    start_points, end_points = [], []
+
+    # vertical start points (constant y)
+    xs = np.linspace(BOARD_BORDER, BOARD_WIDTH - BOARD_BORDER, size)
+    ys = np.full((size), BOARD_BORDER)
+    start_points += list(zip(xs, ys))
+
+    # horizontal start points (constant x)
+    xs = np.full((size), BOARD_BORDER)
+    ys = np.linspace(BOARD_BORDER, BOARD_WIDTH - BOARD_BORDER, size)
+    start_points += list(zip(xs, ys))
+
+    # vertical end points (constant y)
+    xs = np.linspace(BOARD_BORDER, BOARD_WIDTH - BOARD_BORDER, size)
+    ys = np.full((size), BOARD_WIDTH - BOARD_BORDER)
+    end_points += list(zip(xs, ys))
+
+    # horizontal end points (constant x)
+    xs = np.full((size), BOARD_WIDTH - BOARD_BORDER)
+    ys = np.linspace(BOARD_BORDER, BOARD_WIDTH - BOARD_BORDER, size)
+    end_points += list(zip(xs, ys))
+
+    return (start_points, end_points)
+
+
+def xy_to_colrow(x, y, size):
+    """Convert x,y coordinates to column and row number
+    Args:
+        x (float): x position
+        y (float): y position
+        size (int): size of grid
+    Returns:
+        Tuple[int, int]: column and row numbers of intersection
+    """
+    inc = (BOARD_WIDTH - 2 * BOARD_BORDER) / (size - 1)
+    x_dist = x - BOARD_BORDER
+    y_dist = y - BOARD_BORDER
+    col = int(round(x_dist / inc))
+    row = int(round(y_dist / inc))
+    return col, row
+
+
+def colrow_to_xy(col, row, size):
+    """Convert column and row numbers to x,y coordinates
+    Args:
+        col (int): column number (horizontal position)
+        row (int): row number (vertical position)
+        size (int): size of grid
+    Returns:
+        Tuple[float, float]: x,y coordinates of intersection
+    """
+    inc = (BOARD_WIDTH - 2 * BOARD_BORDER) / (size - 1)
+    x = int(BOARD_BORDER + col * inc)
+    y = int(BOARD_BORDER + row * inc)
+    return x, y
+
+
+def has_no_liberties(board, group):
+    """Check if a stone group has any liberties on a given board.
+    Args:
+        board (object): game board (size * size matrix)
+        group (List[Tuple[int, int]]): list of (col,row) pairs defining a stone group
+    Returns:
+        [boolean]: True if group has any liberties, False otherwise
+    """
+    for x, y in group:
+        if x > 0 and board[x - 1, y] == 0:
+            return False
+        if y > 0 and board[x, y - 1] == 0:
+            return False
+        if x < board.shape[0] - 1 and board[x + 1, y] == 0:
+            return False
+        if y < board.shape[0] - 1 and board[x, y + 1] == 0:
+            return False
+    return True
+
+
+def get_stone_groups(board, color):
+    """Get stone groups of a given color on a given board
+    Args:
+        board (object): game board (size * size matrix)
+        color (str): name of color to get groups for
+    Returns:
+        List[List[Tuple[int, int]]]: list of list of (col, row) pairs, each defining a group
+    """
+    size = board.shape[0]
+    color_code = 1 if color == "black" else 2
+    xs, ys = np.where(board == color_code)
+    graph = nx.grid_graph(dim=[size, size])
+    stones = set(zip(xs, ys))
+    all_spaces = set(itertools.product(range(size), range(size)))
+    stones_to_remove = all_spaces - stones
+    graph.remove_nodes_from(stones_to_remove)
+    return nx.connected_components(graph)
+
+
+def is_valid_move(col, row, board):
+    """Check if placing a stone at (col, row) is valid on board
+    Args:
+        col (int): column number
+        row (int): row number
+        board (object): board grid (size * size matrix)
+    Returns:
+        boolean: True if move is valid, False otherewise
+    """
+    # TODO: check for ko situation (infinite back and forth)
+    if col < 0 or col >= board.shape[0]:
+        return False
+    if row < 0 or row >= board.shape[0]:
+        return False
+    return board[col, row] == 0
+
+
+class Gomoku:
+    def __init__(self, size, k=5):
+
+        self.k = k
+        moves = [(x, y) for x in range(1, size + 1)
+                 for y in range(1, size + 1)]
+
         self.board = np.zeros((size, size))
         self.size = size
         self.black_turn = True
         self.start_points, self.end_points = make_grid(self.size)
+
+        self.initial = GameState(to_move=PLAYER_BLACK, utility=0, board=self.board, moves=moves)
+
+    def actions(self, state):
+        """Legal moves are any square not yet taken."""
+        return state.moves
+
+    def result(self, state, move):
+        if move not in state.moves:
+            return state  # Illegal move has no effect
+        board = state.board.copy()
+        board[move] = state.to_move
+        moves = list(state.moves)
+        moves.remove(move)
+        return GameState(to_move=(PLAYER_WHITE if state.to_move == PLAYER_BLACK else PLAYER_BLACK),
+                         utility=self.compute_utility(board, move, state.to_move),
+                         board=board,
+                         moves=moves)
+
+    def utility(self, state, player):
+        """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
+        return state.utility if player == PLAYER_BLACK else -state.utility
+
+    def terminal_test(self, state):
+        """A state is terminal if it is won or there are no empty squares."""
+        return state.utility != 0 or len(state.moves) == 0
+
+    def display(self, state):
+        board = state.board
+        for x in range(1, self.size + 1):
+            for y in range(1, self.size + 1):
+                print(board.get((x, y), '.'), end=' ')
+            print()
+
+    def compute_utility(self, board, move, player):
+        """If 'X' wins with this move, return 1; if 'O' wins return -1; else return 0."""
+        if (self.k_in_row(board, move, player, (0, 1)) or
+                self.k_in_row(board, move, player, (1, 0)) or
+                self.k_in_row(board, move, player, (1, -1)) or
+                self.k_in_row(board, move, player, (1, 1))):
+            return +1 if player == 'X' else -1
+        else:
+            return 0
+
+    def check_endgame(self, board, move, player):
+        x, y = move  # coordinates of the last added stone
+
+        row_start = x - 5 if x - 5 > 0 else 0
+        col_start = y - 5 if y - 5 > 0 else 0
+
+        # Horizontal
+        count_stones = 0
+        for k in range(2 * 5 - 1):
+            if board[x][y + k] == player:
+                count_stones += 1
+            else:
+                count_stones = 0
+
+    def k_in_row(self, board, move, player, delta_x_y):
+        # Check ends of game
+        count_r = count_c = count_d1 = count_d2 = 0
+        for i in range(self.size):
+            for j in range(self.size):
+
+                # Orizzontale
+                if board[i][j] == player:
+                    count_r += 1
+                    if count_r == 5:
+                        self.win()
+                else:
+                    count_r = 0
+
+                # Verticale
+                if board[j][i] == player:
+                    count_c += 1
+                    if count_c == 5:
+                        self.win()
+                else:
+                    count_c = 0
+
+                # Diagonale 1
+                if i + 5 < self.size and j + 5 < self.size:
+                    for k in range(5):
+                        if board[i + k][j + k] == player:
+                            count_d1 += 1
+                        else:
+                            count_d1 = 0
+                            break
+
+                    if count_d1 == 5:
+                        self.win()
+
+                if i + 5 < self.size and j - 5 < self.size:
+                    for k in range(5):
+                        if board[i + k][j - k] == player:
+                            count_d2 += 1
+                        else:
+                            count_d2 = 0
+                            break
+
+                    if count_d2 == 5:
+                        self.win()
+
+        return n >= self.k
 
     def init_pygame(self):
         # Inizializza la partita
@@ -55,12 +306,14 @@ class Game:
         # TODO : End of Game
         self.end()
 
+        # Mossa del BOT
+        if (not self.critical()):
+            self.nuova_mossa(2.0);
+
         # change turns and draw screen
         self.CLICK.play()
-        self.black_turn = not self.black_turn
+        # self.black_turn = not self.black_turn
         self.draw()
-
-
 
     def end(self):
         # Check ends of game
@@ -111,7 +364,83 @@ class Game:
 
     def win(self):
         # TODO: Win
+        exit(1)
         pass
+
+    def critical(self):
+        # Check ends of game
+        count_r = count_c = count_d1 = count_d2 = 0
+        adversarial = 1.0 if self.black_turn else 2.0
+        my_color = 2.0 if self.black_turn else 1.0
+        for i in range(self.size):
+            for j in range(self.size):
+
+                # Verticale
+                if self.board[i][j] == adversarial:
+                    count_r += 1
+                    if count_r == 4:
+                        if (i < 4 and self.board[i - 4][j] == 0):
+                            self.board[i - 4][j] = my_color
+                            return True
+                        elif (self.board[i + 1][j] == 0):
+                            self.board[i + 1][j] = my_color
+                            return True
+
+                else:
+                    count_r = 0
+
+                # Orizzontale
+                if self.board[j][i] == adversarial:
+                    count_c += 1
+                    if count_c == 4:
+                        if (j > 4 and self.board[j - 4][i] == 0):
+                            self.board[j - 4][i] = my_color
+                            return True
+                        elif (j + 1 < self.size and self.board[j + 1][i] == 0):
+                            self.board[j + 1][i] = my_color
+                            return True
+                else:
+                    count_c = 0
+
+                # Diagonale 1
+                if i + 5 < self.size and j + 5 < self.size:
+                    for k in range(4):
+                        if self.board[i + k][j + k] == adversarial:
+                            count_d1 += 1
+                        else:
+                            count_d1 = 0
+                            break
+
+                    if count_d1 == 4:
+                        if (i > 0 and j > 0 and self.board[i - 1][j - 1] == 0):
+                            self.board[i - 1][j - 1] = my_color
+                            return True
+                        elif (i + 4 < self.size and j + 4 < self.size and self.board[i + 4][j + 4] == 0):
+                            self.board[i + 4][j + 4] = my_color
+                            return True
+
+                if i + 5 < self.size and j - 5 < self.size:
+                    for k in range(4):
+                        if self.board[i + k][j - k] == adversarial:
+                            count_d2 += 1
+                        else:
+                            count_d2 = 0
+                            break
+
+                    if count_d2 == 4:
+                        if (i > 0 and j > 0 and self.board[i - 1][j - 1] == 0):
+                            self.board[i - 1][j - 1] = my_color
+                            return True
+                        elif (i < self.size - 1 and j < self.size - 1 and self.board[i + 1][j + 1] == 0):
+                            self.board[i + 1][j + 1] = my_color
+                            return True
+        return False
+
+    def nuova_mossa(self, my_color):
+        v = random.sample(range(1, 15), 2)
+        while (not is_valid_move(v[0], v[1], self.board)):
+            v = random.sample(range(1, 15), 2)
+        self.board[v[0], v[1]] = my_color
 
     def draw(self):
         # draw stones - filled circle and antialiased ring
@@ -135,7 +464,6 @@ class Game:
         pygame.display.flip()
 
     def update(self):
-        # TODO: undo button
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.MOUSEBUTTONUP:
@@ -147,68 +475,12 @@ class Game:
                     self.pass_move()
 
 
+if __name__ == "__main__":
+    g = Game(size=15)
+    g.init_pygame()
+    g.clear_screen()
+    g.draw()
 
-class Gomoku(Game):
-
-    def __init__(self, h=3, v=3, k=3):
-        self.h = h
-        self.v = v
-        self.k = k
-        moves = [(x, y) for x in range(1, h + 1)
-                 for y in range(1, v + 1)]
-        self.initial = GameState(to_move='X', utility=0, board={}, moves=moves)
-
-    def actions(self, state):
-        """Legal moves are any square not yet taken."""
-        return state.moves
-
-    def result(self, state, move):
-        if move not in state.moves:
-            return state  # Illegal move has no effect
-        board = state.board.copy()
-        board[move] = state.to_move
-        moves = list(state.moves)
-        moves.remove(move)
-        return GameState(to_move=('O' if state.to_move == 'X' else 'X'),
-                         utility=self.compute_utility(board, move, state.to_move),
-                         board=board, moves=moves)
-
-    def utility(self, state, player):
-        """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
-        return state.utility if player == 'X' else -state.utility
-
-    def terminal_test(self, state):
-        """A state is terminal if it is won or there are no empty squares."""
-        return state.utility != 0 or len(state.moves) == 0
-
-    def display(self, state):
-        board = state.board
-        for x in range(1, self.h + 1):
-            for y in range(1, self.v + 1):
-                print(board.get((x, y), '.'), end=' ')
-            print()
-
-    def compute_utility(self, board, move, player):
-        """If 'X' wins with this move, return 1; if 'O' wins return -1; else return 0."""
-        if (self.k_in_row(board, move, player, (0, 1)) or
-                self.k_in_row(board, move, player, (1, 0)) or
-                self.k_in_row(board, move, player, (1, -1)) or
-                self.k_in_row(board, move, player, (1, 1))):
-            return +1 if player == 'X' else -1
-        else:
-            return 0
-
-    def k_in_row(self, board, move, player, delta_x_y):
-        """Return true if there is a line through move on board for player."""
-        (delta_x, delta_y) = delta_x_y
-        x, y = move
-        n = 0  # n is number of moves in row
-        while board.get((x, y)) == player:
-            n += 1
-            x, y = x + delta_x, y + delta_y
-        x, y = move
-        while board.get((x, y)) == player:
-            n += 1
-            x, y = x - delta_x, y - delta_y
-        n -= 1  # Because we counted move itself twice
-        return n >= self.k
+    while True:
+        g.update()
+        pygame.time.wait(100)
