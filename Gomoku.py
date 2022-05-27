ca4f7679ad323fffcfe0ed2450ebf85d10937bdd
DESKTOP-JBFT0GQ\vince <vincenzosalvati@hotmail.it>
2022-05-27 11:09:01 +0200

    Remove Warning

--- Gomoku.py
+++ Gomoku.py
@@ -1,5 +1,7 @@
 import itertools
 import sys
+import threading
+import time
 from collections import namedtuple
 
 import numpy as np
@@ -23,14 +25,11 @@ PLAYER_WHITE = 2
 GameState = namedtuple('GameState', 'to_move, utility, board, moves, branching')
 
 
-# Searching
+# noinspection PyShadowingNames
 def alpha_beta_search(game, state):
-    """Search game to determine best action; use alpha-beta pruning.
-    As in [Figure 5.7], this version searches all the way to the leaves."""
-
     player = state.to_move
 
-    # Functions used by alpha_beta
+    # noinspection PyShadowingNames
     def max_value(state, alpha, beta):
         if game.terminal_test(state):
             return game.utility(state, player)
@@ -42,6 +41,7 @@ def alpha_beta_search(game, state):
             alpha = max(alpha, value)
         return value
 
+    # noinspection PyShadowingNames
     def min_value(state, alpha, beta):
         if game.terminal_test(state):
             return game.utility(state, player)
@@ -53,7 +53,6 @@ def alpha_beta_search(game, state):
             beta = min(beta, value)
         return value
 
-    # Body of alpha_beta_search:
     best_score = -np.inf
     beta = np.inf
     best_action = None
@@ -77,28 +76,28 @@ def make_grid(size):
 
     # vertical start points (constant y)
     xs = np.linspace(BOARD_BORDER, BOARD_WIDTH - BOARD_BORDER, size)
-    ys = np.full((size), BOARD_BORDER)
+    ys = np.full(size, BOARD_BORDER)
     start_points += list(zip(xs, ys))
 
     # horizontal start points (constant x)
-    xs = np.full((size), BOARD_BORDER)
+    xs = np.full(size, BOARD_BORDER)
     ys = np.linspace(BOARD_BORDER, BOARD_WIDTH - BOARD_BORDER, size)
     start_points += list(zip(xs, ys))
 
     # vertical end points (constant y)
     xs = np.linspace(BOARD_BORDER, BOARD_WIDTH - BOARD_BORDER, size)
-    ys = np.full((size), BOARD_WIDTH - BOARD_BORDER)
+    ys = np.full(size, BOARD_WIDTH - BOARD_BORDER)
     end_points += list(zip(xs, ys))
 
     # horizontal end points (constant x)
-    xs = np.full((size), BOARD_WIDTH - BOARD_BORDER)
+    xs = np.full(size, BOARD_WIDTH - BOARD_BORDER)
     ys = np.linspace(BOARD_BORDER, BOARD_WIDTH - BOARD_BORDER, size)
     end_points += list(zip(xs, ys))
 
-    return (start_points, end_points)
+    return start_points, end_points
 
 
-def xy_to_colrow(x, y, size):
+def col_row_from(x, y, size):
     """Convert x,y coordinates to column and row number
     Args:
         x (float): x position
@@ -115,7 +114,7 @@ def xy_to_colrow(x, y, size):
     return col, row
 
 
-def colrow_to_xy(col, row, size):
+def x_y_from(col, row, size):
     """Convert column and row numbers to x,y coordinates
     Args:
         col (int): column number (horizontal position)
@@ -130,27 +129,21 @@ def colrow_to_xy(col, row, size):
     return x, y
 
 
-# Useful function
-def is_valid_move(col, row, board):
-    """Check if placing a stone at (col, row) is valid on board
-    Args:
-        col (int): column number
-        row (int): row number
-        board (object): board grid (size * size matrix)
-    Returns:
-        boolean: True if move is valid, False otherewise
-    """
-    # TODO: check for ko situation (infinite back and forth)
-    if col < 0 or col >= board.shape[0]:
-        return False
-    if row < 0 or row >= board.shape[0]:
-        return False
-    return board[col, row] == 0
-
-
 class Gomoku:
+
     def __init__(self, size, length_victory=5):
+        pygame.init()
+
+        self.size = size
         self.length_victory = length_victory
+        self.board = np.zeros((size, size))
+
+        self.screen = pygame.display.set_mode((BOARD_WIDTH, BOARD_WIDTH))
+        self.start_points, self.end_points = make_grid(size)
+        self.stop_drawing = False
+        self.font = pygame.font.SysFont("arial", 30)
+        self.WRONG_CLICK = pygame.mixer.Sound("wav/wrong_click.wav")
+        self.RIGHT_CLICK = pygame.mixer.Sound("wav/right_click.wav")
 
         # Where the game is getting involved
         self.x_min = 16
@@ -158,14 +151,11 @@ class Gomoku:
         self.y_min = 16
         self.y_max = -1
 
-        self.board = np.zeros((size, size))
-        self.size = size
         self.black_turn = True
-        self.start_points, self.end_points = make_grid(self.size)
 
-        moves = [(x, y) for x in range(size)
+        moves = [(x, y)
+                 for x in range(size)
                  for y in range(size)]
-        self.moves_done = []
 
         self.initial = GameState(to_move=(PLAYER_BLACK if self.black_turn else PLAYER_WHITE),
                                  utility=0,
@@ -173,17 +163,28 @@ class Gomoku:
                                  moves=moves,
                                  branching=3)
 
-    def init_pygame(self):
-        pygame.init()
-        screen = pygame.display.set_mode((BOARD_WIDTH, BOARD_WIDTH))
-        self.screen = screen
-        self.ZOINK = pygame.mixer.Sound("wav/zoink.wav")
-        self.CLICK = pygame.mixer.Sound("wav/click.wav")
-        self.font = pygame.font.SysFont("arial", 30)
-
-    def actions(self, state):
+    @staticmethod
+    def actions(state):
         """Legal moves are any square not yet taken."""
         return state.moves
+
+    @staticmethod
+    def utility(state, player):
+        """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
+        return state.utility if player == PLAYER_BLACK else -state.utility
+
+    @staticmethod
+    def terminal_test(state):
+        """A state is terminal if it is won or there are no empty squares."""
+        return state.utility != 0 or len(state.moves) == 0 or state.branching == 0
+
+    @staticmethod
+    def to_move(state):
+        """Return the player whose move it is in this state."""
+        return state.to_move
+
+    def pass_move(self):
+        self.black_turn = not self.black_turn
 
     def compute_moves(self, board):
 
@@ -202,103 +203,50 @@ class Gomoku:
 
     def result(self, state, move):
         if move not in state.moves:
-            return state  # Illegal move has no effect
+            return state
+
         board = state.board.copy()
-
         board[move] = state.to_move
-
         moves = self.compute_moves(board)
+
         return GameState(to_move=(PLAYER_BLACK if self.black_turn else PLAYER_WHITE),
-                         utility=self.compute_utility(board, move, state.to_move),
+                         utility=self.compute_utility(board),
                          board=board,
                          moves=moves,
                          branching=state.branching - 1)
 
-    def utility(self, state, player):
-        """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
-        return state.utility if player == PLAYER_BLACK else -state.utility
-
-    def terminal_test(self, state):
-        """A state is terminal if it is won or there are no empty squares."""
-        return state.utility != 0 or len(state.moves) == 0 or state.branching == 0
-
-    def to_move(self, state):
-        """Return the player whose move it is in this state."""
-        return state.to_move
-
-    def pass_move(self):
-        self.black_turn = not self.black_turn
-        self.draw()
-
     def extract_arrays(self, board):
-        diags = []
         transpose_board = np.transpose(board)
         flipped_board = np.flip(board, 1)
-        flipped_transposed = np.transpose(flipped_board)
+        flipped_transposed_board = np.transpose(flipped_board)
 
         rows = [board[_, :] for _ in range(board.shape[0])]
         cols = [board[:, _] for _ in range(board.shape[1])]
-
-        # Extract all diagonals
+        diagonals = []
         for j in range(self.size - self.length_victory + 1):
-            diags.append(np.diag(board[0:, j:]))
-            diags.append(np.diag(flipped_board[0:, j:]))
+            diagonals.append(np.diag(board[0:, j:]))
+            diagonals.append(np.diag(flipped_board[0:, j:]))
             if j != 0:
-                diags.append(np.diag(transpose_board[0:, j:]))
-                diags.append(np.diag(flipped_transposed[0:, j:]))
+                diagonals.append(np.diag(transpose_board[0:, j:]))
+                diagonals.append(np.diag(flipped_transposed_board[0:, j:]))
 
-        return [*rows, *cols, *diags]
+        return [*rows, *cols, *diagonals]
 
-    def subarray(self, array, length_subarray):
-        return np.fromfunction(lambda i, j: array[i + j], (len(array) - length_subarray + 1, length_subarray),
-                               dtype=int)
+    @staticmethod
+    def subarray(array, length_subarray):
+        return [*np.fromfunction(lambda i, j: array[i + j], (len(array) - length_subarray + 1, length_subarray),
+                                 dtype=int)]
 
-    def evaluate_line(self, array):
-        lines = self.subarray(array, 5)
-        res = {}
-        # t_fiveInRow = Thread(target=self.checkFiveInRow, args=(array, res))
-        # t_fourInRow = Thread(target=self.checkFourInRow, args=(array, res))
-        # t_brokenFour = Thread(target=self.checkBrokenFour, args=(array, res))
-        # t_threeInRow = Thread(target=self.checkThreeInRow, args=(array, res))
-        # t_brokenThree = Thread(target=self.checkBrokenThree, args=(array, res))
-        #
-        # t_fiveInRow.start()
-        # t_fourInRow.start()
-        # t_brokenFour.start()
-        # t_threeInRow.start()
-        # t_brokenThree.start()
-        #
-        # t_fiveInRow.join()
-        # t_fourInRow.join()
-        # t_brokenFour.join()
-        # t_threeInRow.join()
-        # t_brokenThree.join()
-
-        self.checkFiveInRow(lines, res)
-        self.checkFourInRow(lines, res)
-        self.checkBrokenFour(lines, res)
-        self.checkThreeInRow(lines, res)
-        self.checkBrokenThree(lines, res)
-        self.checkTwoInRow(lines, res)
-        self.checkBrokenTwo(lines, res)
-        self.checkOnes(lines, res)
-
-        return 1 * res["FiveInRow"] + \
-               0.6 * res["FourInRow"] + \
-               0.6 * res["BrokenFour"] + \
-               0.3 * res["ThreeInRow"] + \
-               0.3 * res["BrokenThree"] + \
-               0.05 * res["TwoInRow"] + \
-               0.001 * res["Ones"]
-
-    def checkFiveInRow(self, lines, res):
+    @staticmethod
+    def check_five_in_row(lines, res):
         count = 0
         for line in lines:
             if np.all(line == [1, 1, 1, 1, 1]):
                 count += 1
         res["FiveInRow"] = count
 
-    def checkFourInRow(self, lines, res):
+    @staticmethod
+    def check_four_in_row(lines, res):
         count = 0
         for line in lines:
             if np.all(line == [0, 1, 1, 1, 1]) or \
@@ -306,7 +254,8 @@ class Gomoku:
                 count += 1
         res["FourInRow"] = count
 
-    def checkBrokenFour(self, lines, res):
+    @staticmethod
+    def check_broken_four(lines, res):
         count = 0
         for line in lines:
             if np.all(line == [1, 0, 1, 1, 1]) or \
@@ -315,26 +264,29 @@ class Gomoku:
                 count += 1
         res["BrokenFour"] = count
 
-    def checkThreeInRow(self, lines, res):
+    @staticmethod
+    def check_three_in_row(lines, res):
         count = 0
         for line in lines:
-            if np.all(line == [0, 1, 1, 1, 0]) or \
-                    np.all(line == [1, 1, 1, 0, 0]) or \
+            if np.all(line == [1, 1, 1, 0, 0]) or \
+                    np.all(line == [0, 1, 1, 1, 0]) or \
                     np.all(line == [0, 0, 1, 1, 1]):
                 count += 1
         res["ThreeInRow"] = count
 
-    def checkBrokenThree(self, lines, res):
+    @staticmethod
+    def check_broken_three(lines, res):
         count = 0
         for line in lines:
-            if np.all(line == [0, 1, 0, 1, 1]) or \
-                    np.all(line == [0, 1, 1, 0, 1]) or \
+            if np.all(line == [1, 0, 1, 1, 0]) or \
+                    np.all(line == [0, 1, 0, 1, 1]) or \
                     np.all(line == [1, 1, 0, 1, 0]) or \
-                    np.all(line == [1, 0, 1, 1, 0]):
+                    np.all(line == [0, 1, 1, 0, 1]):
                 count += 1
         res["BrokenThree"] = count
 
-    def checkTwoInRow(self, lines, res):
+    @staticmethod
+    def check_two_in_row(lines, res):
         count = 0
         for line in lines:
             if np.all(line == [1, 1, 0, 0, 0]) or \
@@ -344,7 +296,8 @@ class Gomoku:
                 count += 1
         res["TwoInRow"] = count
 
-    def checkBrokenTwo(self, lines, res):
+    @staticmethod
+    def check_broken_two(lines, res):
         count = 0
         for line in lines:
             if np.all(line == [1, 0, 1, 0, 0]) or \
@@ -353,7 +306,8 @@ class Gomoku:
                 count += 1
         res["BrokenTwo"] = count
 
-    def checkOnes(self, lines, res):
+    @staticmethod
+    def check_one(lines, res):
         count = 0
         for line in lines:
             if np.all(line == [1, 0, 0, 0, 0]) or \
@@ -364,8 +318,28 @@ class Gomoku:
                 count += 1
         res["Ones"] = count
 
-    def compute_utility(self, board, move, player):
-        """If 'X' wins with this move, return 1; if 'O' wins return -1; else return 0."""
+    def evaluate_line(self, array):
+        lines = self.subarray(array, 5)
+        count_dict = {}
+
+        self.check_five_in_row(lines, count_dict)
+        self.check_four_in_row(lines, count_dict)
+        self.check_broken_four(lines, count_dict)
+        self.check_three_in_row(lines, count_dict)
+        self.check_broken_three(lines, count_dict)
+        self.check_two_in_row(lines, count_dict)
+        self.check_broken_two(lines, count_dict)
+        self.check_one(lines, count_dict)
+
+        return 1 * count_dict["FiveInRow"] + \
+               0.6 * count_dict["FourInRow"] + \
+               0.6 * count_dict["BrokenFour"] + \
+               0.3 * count_dict["ThreeInRow"] + \
+               0.3 * count_dict["BrokenThree"] + \
+               0.05 * count_dict["TwoInRow"] + \
+               0.001 * count_dict["Ones"]
+
+    def compute_utility(self, board):
         arrays = self.extract_arrays(board)
         score = 0
         for array in arrays:
@@ -383,7 +357,6 @@ class Gomoku:
             self.y_max = y
 
     def end(self, player, move):
-        # Check ends of game
         x, y = move
         x = x + 5
         y = y + 5
@@ -392,11 +365,11 @@ class Gomoku:
 
         clipped_row = padded[x - 5:x + 6, y]
         clipped_col = padded[x, y - 5:y + 6]
-        clipped_diag = np.diag(padded[x - 5:x + 6, y - 5:y + 6])
-        clipped_diag2 = np.diag(np.flip(padded[x - 5:x + 6, y - 5:y + 6], 1))
+        clipped_diagonals = np.diag(padded[x - 5:x + 6, y - 5:y + 6])
+        clipped_flipped_diagonals = np.diag(np.flip(padded[x - 5:x + 6, y - 5:y + 6], 1))
 
         count_stones = 0
-        for clip in [clipped_row, clipped_col, clipped_diag, clipped_diag2]:
+        for clip in [clipped_row, clipped_col, clipped_diagonals, clipped_flipped_diagonals]:
             for element in clip:
                 if element == player:
                     count_stones += 1
@@ -404,7 +377,6 @@ class Gomoku:
                     break
                 else:
                     count_stones = 0
-
             if count_stones == 5:
                 self.win()
             else:
@@ -412,16 +384,16 @@ class Gomoku:
 
     def win(self):
         # TODO: Win
-        exit(1)
+        self.stop_drawing = True
+        pygame.quit()
+        sys.exit()
         pass
 
     def critical(self, player):
-        arrays = self.extract_arrays(self.board)
-        for array in arrays:
-            lines = self.subarray(array, 5)
-            for line in lines:
+        for array in self.extract_arrays(self.board):
+            for line in self.subarray(array, 5):
                 if np.all(line == [0, player, player, player, player]) or \
-                        np.all(line[0] == [player, player, player, player, 0]) or \
+                        np.all(line == [player, player, player, player, 0]) or \
                         np.all(line == [player, 0, player, player, player]) or \
                         np.all(line == [player, player, 0, player, player]) or \
                         np.all(line == [player, player, player, 0, player]):
@@ -430,7 +402,6 @@ class Gomoku:
         return False
 
     def bot_move(self):
-        # BOT move
         if not self.critical(PLAYER_BLACK if self.black_turn else PLAYER_WHITE):
             state = GameState(to_move=(PLAYER_BLACK if self.black_turn else PLAYER_WHITE),
                               utility=0,
@@ -444,16 +415,23 @@ class Gomoku:
 
             # draw stone, play sound, check end and pass move
             self.board[col_bot, row_bot] = PLAYER_WHITE
-            self.CLICK.play()
+            self.RIGHT_CLICK.play()
             self.end(PLAYER_WHITE, (col_bot, row_bot))
             self.pass_move()
+
+    def is_valid_move(self, col, row):
+        if col < 0 or col >= self.size:
+            return False
+        if row < 0 or row >= self.size:
+            return False
+        return self.board[col, row] == 0
 
     def handle_click(self):
         # get board position
         x, y = pygame.mouse.get_pos()
-        col, row = xy_to_colrow(x, y, self.size)
-        if not is_valid_move(col, row, self.board):
-            self.ZOINK.play()
+        col, row = col_row_from(x, y, self.size)
+        if not self.is_valid_move(col, row):
+            self.WRONG_CLICK.play()
             return
 
         # range game coordinates
@@ -461,7 +439,7 @@ class Gomoku:
 
         # draw stone, play sound, check end and pass move
         self.board[col, row] = PLAYER_BLACK
-        self.CLICK.play()
+        self.RIGHT_CLICK.play()
         self.end(PLAYER_BLACK, (col, row))
         self.pass_move()
         self.bot_move()
@@ -482,21 +460,21 @@ class Gomoku:
         # add guide dots
         guide_dots = [3, self.size // 2, self.size - 4]
         for col, row in itertools.product(guide_dots, guide_dots):
-            x, y = colrow_to_xy(col, row, self.size)
+            x, y = x_y_from(col, row, self.size)
             gfxdraw.aacircle(self.screen, x, y, DOT_RADIUS, BLACK)
             gfxdraw.filled_circle(self.screen, x, y, DOT_RADIUS, BLACK)
 
         pygame.display.flip()
 
     def draw(self):
-        # draw stones - filled circle and antialiased ring
         self.clear_screen()
+
         for col, row in zip(*np.where(self.board == 1)):
-            x, y = colrow_to_xy(col, row, self.size)
+            x, y = x_y_from(col, row, self.size)
             gfxdraw.aacircle(self.screen, x, y, STONE_RADIUS, BLACK)
             gfxdraw.filled_circle(self.screen, x, y, STONE_RADIUS, BLACK)
         for col, row in zip(*np.where(self.board == 2)):
-            x, y = colrow_to_xy(col, row, self.size)
+            x, y = x_y_from(col, row, self.size)
             gfxdraw.aacircle(self.screen, x, y, STONE_RADIUS, WHITE)
             gfxdraw.filled_circle(self.screen, x, y, STONE_RADIUS, WHITE)
 
@@ -514,6 +492,7 @@ class Gomoku:
 
         for event in events:
             if event.type == pygame.QUIT:
+                self.stop_drawing = True
                 pygame.quit()
                 sys.exit()
 
@@ -528,11 +507,18 @@ class Gomoku:
         pygame.event.clear()
 
 
+def update_screen():
+    while True:
+        game.draw()
+        time.sleep(0.2)
+        if game.stop_drawing:
+            break
+
+
 if __name__ == "__main__":
     game = Gomoku(15)
-    game.init_pygame()
-    game.draw()
-
+    thread_draw = threading.Thread(target=update_screen, args=())
+    thread_draw.start()
     while True:
         game.update()
         pygame.time.wait(100)
