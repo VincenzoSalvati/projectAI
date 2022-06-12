20fb3a9ef4058814acc6bf3bf7059479eb1e2c55
EnzoKalk <45711698+EnzoKalk@users.noreply.github.com>
2022-06-12 17:32:22 +0200

    Add comments

--- bot/alpha_beta_pruning.py
+++ bot/alpha_beta_pruning.py
@@ -1,3 +1,20 @@
+"""
+Course: Artificial Intelligence 2021/2022
+
+Lecturer:
+Marcelli      Angelo      amarcelli@unisa.it
+Della Cioppa  Antonio     adellacioppa@unisa.it
+
+Group:
+Salvati       Vincenzo    0622701550      v.salvati10@studenti.unisa.it
+Mansi         Paolo       0622701542      p.mansi5@studenti.unisa.it
+
+@file alpha_beta_pruning.py
+
+
+PURPOSE OF THE FILE: search within state spaces.
+"""
+
 import numpy as np
 
 
